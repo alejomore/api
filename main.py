da16baa7f79ea4dfff8181432f3c4029ca19d5dc
Alejandro <aandradez094@gmail.com>
2026-06-29 17:45:07 -0500

    Modificaciones para que funcione

--- main.py
+++ main.py
@@ -2,26 +2,26 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}, {"item_name": "Qux"}, {"item_name": "Quux"}, {"item_name": "Corge"}, {"item_name": "Grault"}, {"item_name": "Garply"}, {"item_name": "Waldo"}, {"item_name": "Fred"}, {"item_name": "Plugh"}, {"item_name": "Xyzzy"}, {"item_name": "Thud"}]
+fake_items_db = [
+    {"item_name": "Foo"},
+    {"item_name": "Bar"},
+    {"item_name": "Baz"},
+]
 
 def mimensaje():
-    return "¡Hola, Fast API!"
-
-def read_root():
-    return {"message": mimensaje()}
+    return "¡Hola, FastAPI!"
 
 @app.get("/")
-def read_root():
-    return read_root()
-
+def root():
+    return {"message": mimensaje()}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
-   return {"item_id": item_id}
+    return {"item_id": item_id}
 
 @app.get("/items/")
-def read_item(skip: int = 0, limit: int = 10, q: str | None = None):
-   results = fake_items_db[skip : skip + limit]
-   if q:
-      results.append({"item_name": q})
-   return results
+def read_items(skip: int = 0, limit: int = 10, q: str | None = None):
+    results = fake_items_db[skip: skip + limit]
+    if q:
+        results.append({"item_name": q})
+    return results
