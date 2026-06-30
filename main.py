f39b8964f423ba1c15527c144fd5a8da3e4946bf
Alejandro <aandradez094@gmail.com>
2026-06-29 21:12:01 -0500

    corrección en list of items

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-fake_items_db = [
-    {"item_name": "Foo"},
-    {"item_name": "Bar"},
-    {"item_name": "Baz"},
-]
+fake_items_db = fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}, {"item_name": "Qux"}, {"item_name": "Quux"}, {"item_name": "Corge"}, {"item_name": "Grault"}, {"item_name": "Garply"}, {"item_name": "Waldo"}, {"item_name": "Fred"}, {"item_name": "Plugh"}, {"item_name": "Xyzzy"}, {"item_name": "Thud"}]
 
 def mimensaje():
     return "¡Hola, FastAPI!"
